0006a6e0e4c0cd0dabd45aa385ab099ed6c9b39d
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-01-22 23:33:13 +0200

    Added new files

---
+++ HTML-Tasks/Add-Title-To-Head/src/js/app.js
@@ -0,0 +1,14 @@
+import "../scss/app.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // This block will be executed once the page is loaded and ready
+
+  setTimeout(() => {
+    window.document.title = 'One new message'
+  }, 3000);
+
+  const button = document.querySelector(".button");
+  button.addEventListener("click", () => {
+    alert("💣");
+  });
+});
